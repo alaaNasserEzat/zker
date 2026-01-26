4e493ca60b14b5519a4217b09d3a011b8e1a6e0a
Aya Nasser <ayanasser1853@gmail.com>
2026-01-26 20:10:37 +0200

    add share in azkar feature

--- lib/features/azkar_feature/presentation/views/widgets/azkar_detail_container.dart
+++ lib/features/azkar_feature/presentation/views/widgets/azkar_detail_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:zker/core/utils/app_colors.dart';
 import 'package:zker/core/utils/app_text_styles.dart';
 import 'package:zker/features/azkar_feature/domain/entites/azkar_item_entity.dart';
@@ -6,7 +7,7 @@ import 'package:zker/features/home_feature/presentation/views/widgets/custom_btn
 
 class AzkarDetailContainer extends StatefulWidget {
   const AzkarDetailContainer({super.key, required this.azkarItemEntity});
-final AzkarItemEntity azkarItemEntity;
+  final AzkarItemEntity azkarItemEntity;
 
   @override
   State<AzkarDetailContainer> createState() => _AzkarDetailContainerState();
@@ -15,54 +16,81 @@ final AzkarItemEntity azkarItemEntity;
 class _AzkarDetailContainerState extends State<AzkarDetailContainer> {
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: AppColors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      offset: Offset(.5, .5),
-                      blurRadius: 10,
-                    ),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 5,
-                    children: [
-                    Row(
-                      children: [
-                        Icon(Icons.share,color: AppColors.mainColor,),
-                        SizedBox(width: 10,),
-                        Icon(Icons.favorite,color: AppColors.mainColor,),
-                        Spacer(),
-                        CircleAvatar(backgroundColor: AppColors.darkPink,radius: 20,child: Text(widget.azkarItemEntity.id.toString(),style: AppTextStyles.titles.copyWith(color: AppColors.mainColor),),)
-                      ],
-                    ),
-                    Text(
-                      textDirection: TextDirection.rtl,    
-                      widget.azkarItemEntity.text
-                      //     "﴿اللَّهُ لَا إِلَٰهَ إِلَّا هُوَ الْحَيُّ الْقَيُّومُ ۚ لَا تَأْخُذُهُ سِنَةٌ وَلَا نَوْمٌ ۚ لَهُ مَا فِي السَّمَاوَاتِ وَمَا فِي الْأَرْضِ ۗ مَن ذَا الَّذِي يَشْفَعُ عِندَهُ إِلَّا بِإِذْنِهِ ۚ يَعْلَمُ مَا بَيْنَ أَيْدِيهِمْ وَمَا خَلْفَهُمْ ۖ وَلَا يُحِيطُونَ بِشَيْءٍ مِّنْ عِلْمِهِ إِلَّا بِمَا شَاءَ ۚ وَسِعَ كُرْسِيُّهُ السَّمَاوَاتِ وَالْأَرْضَ ۖ وَلَا يَئُودُهُ حِفْظُهُمَا ۚ وَهُوَ الْعَلِيُّ الْعَظِيمُ﴾"
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: AppColors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(.5, .5),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 5,
+            children: [
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      SharePlus.instance.share(
+                        ShareParams(
+                          text: widget.azkarItemEntity.text,
+                        ),
+                      );
+                    },
+                    child: Icon(Icons.share, color: AppColors.mainColor),
+                  ),
+
+                  SizedBox(width: 10),
+                  Icon(Icons.favorite, color: AppColors.mainColor),
+                  Spacer(),
+                  CircleAvatar(
+                    backgroundColor: AppColors.darkPink,
+                    radius: 20,
+                    child: Text(
+                      widget.azkarItemEntity.id.toString(),
+                      style: AppTextStyles.titles.copyWith(
+                        color: AppColors.mainColor,
                       ),
-                    Divider(color: AppColors.mainColor,radius: BorderRadius.circular(16),),
-                    CustomBtn(title: "${widget.azkarItemEntity.currCount}/${widget.azkarItemEntity.count}", onTap: () {  
-                  setState(() {
-                    
-                        if(widget.azkarItemEntity.currCount<widget.azkarItemEntity.count){
-                        widget.azkarItemEntity.currCount+=1;
-                        
-                      }
-                      print("+++++++++++++++++++++++++++++${widget.azkarItemEntity.currCount}");
-                  });
-                    },)
-                    ]),
-                ),
+                    ),
+                  ),
+                ],
               ),
+              Text(
+                textDirection: TextDirection.rtl,
+                widget.azkarItemEntity.text,
+              ),
+              Divider(
+                color: AppColors.mainColor,
+                radius: BorderRadius.circular(16),
+              ),
+              CustomBtn(
+                title:
+                    "${widget.azkarItemEntity.currCount}/${widget.azkarItemEntity.count}",
+                onTap: () {
+                  setState(() {
+                    if (widget.azkarItemEntity.currCount <
+                        widget.azkarItemEntity.count) {
+                      widget.azkarItemEntity.currCount += 1;
+                    }
+                    print(
+                      "+++++++++++++++++++++++++++++${widget.azkarItemEntity.currCount}",
+                    );
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
