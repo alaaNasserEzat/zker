0ff0e60d13d7cd10e851e68903ccfa2bc4b4ae60
Aya Nasser <ayanasser1853@gmail.com>
2026-08-06 16:59:44 +0300

    add share image method  of sunnah of friday

--- lib/features/friday_sunnah_feature/presentation/views/widgets/friday_sunah_header.dart
+++ lib/features/friday_sunnah_feature/presentation/views/widgets/friday_sunah_header.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:zker/core/constent/extensions/localelization_extention.dart';
-import 'package:zker/core/utils/app_colors.dart';
-import 'package:zker/core/utils/app_text_styles.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+import 'dart:io';
 
 class FridaySunnahHeader extends StatelessWidget {
   const FridaySunnahHeader({super.key});
+
+  Future<void> _shareImage() async {
+    final byteData = await rootBundle.load(
+      'assets/images/friday_sunnah_imag.jpeg',
+    );
+
+    final tempDir = await getTemporaryDirectory();
+
+    final file = File('${tempDir.path}/friday_sunnah_imag.jpeg');
+
+    await file.writeAsBytes(byteData.buffer.asUint8List());
+
+    await SharePlus.instance.share(ShareParams(files: [XFile(file.path)]));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: ClipRRect(
+      child: InkWell(
         borderRadius: BorderRadius.circular(24),
-        child: Image.asset(
-          "assets/images/friday_sunnah_imag.jpeg",
-          fit: BoxFit.cover,
+        onTap: _shareImage,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(24),
+          child: Image.asset(
+            'assets/images/friday_sunnah_imag.jpeg',
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
